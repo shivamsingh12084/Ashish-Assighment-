ab124954cacfed3d363bfaa7eba0f9a0b2b41c6b
Shivam Kumar <shivamsingh12084@gmail.com>
2023-04-22 17:22:35 +0530

    Modeified project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import logo from './logo.svg';
+
+import React from "react";
 import './App.css';
 
-import Welcome from './components/Welcome';
-import { useState } from 'react';
+
+import List from "./components/List";
 
 function App() {
-  const [isShown, setIsShown] = useState(false);
-
-  const [name , setName] = useState('')
-
-  const handleClick = event => {
-   
-    setIsShown(true);
-
-  }
-  const handleChange = (event) => {
-    setName(event.target.value);
-    setIsShown(false)
-  };
 
   return (
     <div>
-        <h1>Web Development Assignment</h1>
-        <p>Enter you name: <input value={name} onChange={handleChange}>
-
-        </input> <button type='submit' onClick={handleClick}>Login</button></p>
-
-        {/* 👇️ show elements on click */}
-        {isShown && <Welcome name={name}/>}
-        <p>Assignment done by Ashish Kumar Singh(12015585)</p>
+      <List />
+     
     </div>
   );
 }
 
 export default App;
+
